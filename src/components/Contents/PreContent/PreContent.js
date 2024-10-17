4b224213be0479e5f55a1625d276d54169987533
mailyhai <mailyhai814@gmal.com>
2024-10-17 21:56:59 +0700

    update code

--- src/components/Contents/PreContent/PreContent.js
+++ src/components/Contents/PreContent/PreContent.js
@@ -4,22 +4,19 @@ import "./PreContent.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faX } from "@fortawesome/free-solid-svg-icons";
 
-
 export default function PreContent() {
-
   const [droppedItems, setDroppedItems] = useState([]);
 
-  // Xóa item theo index 
+  // Xóa item theo index
   function deleteItem(index) {
     // Tao mang moi tru phan tu co key la index
-    const newItems = droppedItems.filter(function(item, i) {
+    const newItems = droppedItems.filter(function (item, i) {
       return i !== index; // giu lai phan tu ko phai la index
     });
-  
+
     // cpa nhat lai mang
     setDroppedItems(newItems);
   }
-
 
   // xoa toan bo item trong mang
   const deleteAll = () => {
@@ -34,26 +31,28 @@ export default function PreContent() {
     }
   }, [droppedItems]);
 
-
   // Show content
   function showcontentbtn(item) {
-    const show = document.getElementById('showcontent');
+    const show = document.getElementById("showcontent");
 
-    show.querySelector('h3').textContent = `Thẻ: ${item.name}`;
-    show.querySelector('p').textContent = item.content || "Không có nội dung";
-    
-    if (show.classList.contains('show')) {
-      show.classList.remove('show'); 
+    show.querySelector("h3").textContent = `Thẻ: ${item.name}`;
+    show.querySelector("p").textContent = item.content || "Không có nội dung";
+
+    if (show.classList.contains("show")) {
+      show.classList.remove("show");
     } else {
-      show.classList.add('show'); 
+      show.classList.add("show");
     }
   }
 
   function exit() {
-    const show = document.getElementById('showcontent');
-    show.classList.remove('show');
+    const show = document.getElementById("showcontent");
+    show.classList.remove("show");
   }
-  
+
+  useEffect(() => {
+    console.log(droppedItems);
+  }, [droppedItems]);
   return (
     <div className="pre_content">
       <h3 className="keothe">Kéo thẻ vào đây</h3>
@@ -61,7 +60,7 @@ export default function PreContent() {
       <ReactSortable
         list={droppedItems}
         setList={setDroppedItems}
-        group={{ name: "shared", pull: false, put: true }}
+        group={{ name: "shared", pull: false, put: true, swap: "true" }}
         className="dropitems"
       >
         {droppedItems.map((item, index) => (
@@ -80,15 +79,17 @@ export default function PreContent() {
               <input type="text" id={index} placeholder="Nhập nội dung" />
             </div>
             <div className="itemcontent">
-              <button className="btn" onClick={() => showcontentbtn(item)}>Mô Tả</button> 
+              <button className="btn" onClick={() => showcontentbtn(item)}>
+                Mô Tả
+              </button>
             </div>
           </div>
         ))}
       </ReactSortable>
       <div id="showcontent" className="showcontent">
-      <FontAwesomeIcon className="x" onClick={exit}  icon={faX} />
-        <h3 className="title"></h3> 
-        <p className="pcontent"></p>        
+        <FontAwesomeIcon className="x" onClick={exit} icon={faX} />
+        <h3 className="title"></h3>
+        <p className="pcontent"></p>
       </div>
     </div>
   );
